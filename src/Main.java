73c7bd93da1b8c7fee599ba4c58d01c6263f0b50
Alexi Themistocleous <AlexiThemistocleous@Alexis-MBP.lan>
2018-11-17 12:57:20 +0000

    Adding missing files

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+
+public class Main {
+	
+	public static void main(String[] args) {
+		BattleSequencer battler = new BattleSequencer();
+		
+		//nice words
+		
+		// make character
+		MainCharacter yeetyboi = new MainCharacter();
+		yeetyboi.CreateStats(); 
+		
+		// nice words 
+		
+		// make decisions
+		
+		// make enemy
+		SimpleEnemy YoteyBoi = new SimpleEnemy();
+		// fight them
+		
+		
+	}
+
+}
---
+++ src/MainCharacter.java
@@ -0,0 +1,49 @@
+public class MainCharacter implements Interfaces.IMainCharacter{
+	
+	private int Health = 0;
+	private int Skill = 0;
+	private int baseSkill = 4;
+	private int baseHealth = 4;
+	private int maxSkill = 0;
+	private int maxHealth = 0;
+	
+	public MainCharacter() {
+	}
+	
+	public void CreateStats() {
+		System.out.println("roll the dice to determine your health");
+		int firstHealthRoll = DiceRoller.RollDice();
+		int secondHealthRoll = DiceRoller.RollDice();
+		Health = baseHealth + firstHealthRoll + secondHealthRoll;
+		System.out.println("Health = " + Health);
+		maxHealth = Health;
+		
+		System.out.println("roll the dice to determine your skill");
+		int firstSkillRoll = DiceRoller.RollDice();
+		int secondSkillRoll = DiceRoller.RollDice();
+		Skill = baseSkill + firstSkillRoll + secondSkillRoll;
+		System.out.println("Skill = " + Skill);
+		maxSkill = Skill;		
+	}
+
+	public int GetSkill() {
+		return Skill;
+	}
+
+	@Override
+	public int GetHealth() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void LoseHealth() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean IsDead() {
+		// TODO Auto-generated method stub
+		return false;
+	}

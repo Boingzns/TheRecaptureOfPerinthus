e95d4b7bcad1f00840aabfd115c7d5281717b0c4
Boingzns <zebsummerfield@gmail.com>
2018-11-17 14:45:18 +0000

    Update src/Main.java

--- src/Main.java
+++ src/Main.java
@@ -13,7 +13,7 @@ public class Main {
 		
 		//nice words
 		System.out.println("The burned city lays before you, its once greatness ruined to the scars of war.");
-		System.out.println("the general lies within the castle, but first you need to penetrate the walled city");
+		System.out.println("the general lies within the castle, but first you need to penetrate the walled city.");
 		System.out.println("Due to the recent capture the city is in lockdown and guards patrol the perimeter wall and the gates;");
 		System.out.println("Will you: A) Start to circle the wall searching for an alternative way in");
 		System.out.println(" B) Impersonate a farmer trying to seek refuge in the city");
@@ -25,10 +25,14 @@ public class Main {
 		}
 		//Make referral to next page in accordance with choice
 		else if (theirChoice.equals("B")) {
-			System.out.println("");
+			System.out.println("You put on your rags and straw hat that you keep for times when anonimity is required for your tasks,");
+			System.out.println("You aproach the entrance playing the part of the old farmer with the crooked back; the gate seems strangely sparse of civilians");
+			System.out.println("One of the guards confronts you 5 metres infront of the aperture, an inexperienced militia conscripted only for the war");
 		}
 		//Make referral to next page in accordance with choice
-		
+		else if (theirChoice.equals("C")) {
+			System.out.println("");
+		}
 
 		// nice words 
 		

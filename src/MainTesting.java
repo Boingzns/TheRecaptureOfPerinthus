ba228206eb33ca1843a4ac8d6917d0bf4cf9cd38
Boingzns <zebsummerfield@gmail.com>
2018-11-17 13:54:05 +0000

    gg stuf

--- src/MainTesting.java
+++ src/MainTesting.java
@@ -2,7 +2,15 @@
 public class MainTesting {
 
 	public static void main(String[] args) {
+		Inventory myThings = new Inventory();
 		
+		myThings.StoreItem(new InventoryItem("Sword Of Destiny", "its like a sword, but for destinies"));
+		
+		InventoryItem swordOfDestiny = myThings.GetItem("Sword Of Destiny");
+		
+		
+		System.out.println(swordOfDestiny.Name);
+		System.out.println(swordOfDestiny.Description);
 	}
 
 }

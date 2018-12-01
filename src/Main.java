a85992bf7480642f08e89d5a7048943dc14f6365
Boingzns <zebsummerfield@gmail.com>
2018-12-01 14:19:59 +0000

    Update Main.java
    main story changes

--- src/Main.java
+++ src/Main.java
@@ -21,14 +21,58 @@ public class Main {
 		String theirChoice = input.nextLine();
 		
 		if (theirChoice.equals("A")) {
-			System.out.println("");
+			LookAroundFindSewer();
 		}
 		//Make referral to next page in accordance with choice
 		else if (theirChoice.equals("B")) {
 			System.out.println("You put on your rags and straw hat that you keep for times when anonimity is required for your tasks,");
-			System.out.println("You aproach the entrance playing the part of the old farmer with the crooked back; the gate seems strangely sparse of civilians");
+			System.out.println("You aproach the entrance playing the part of the old farmer with a crooked back; the gate seems strangely sparse of civilians");
 			System.out.println("One of the guards confronts you 5 metres infront of the aperture, an inexperienced militia conscripted only for the war");
+			System.out.println("He tells you that the gate is closed and to turn around before he takes you into custody");
+			System.out.println("Will you: A) try to persuade the militia of your need to enter the city");
+			System.out.println("B) fight your way through"),
+			System.out.println("C) walk away from the gate and look for an alternative way into the city");
+			String theirChoice2 = input.nextLine();
+			
+			if (theirChoice2.equals("A")) {
+				System.out.println("you tell the guard of how your village was burned down by brigands and your family killed");
+				System.out.println("and you plead for him to let you seek refuge in the city so you can pray at the grand temple");
+				System.out.println("The guard looks at you with the same hard stern and tells you to beat it before he draws his sword");
+				System.out.println("Will you: A) fight your way through");
+				System.out.println("B) walk away from the gate and look for an alternative way into the city");
+				String theirChoice3 = input.nextLine();
+				
+				if (theirChoice3.equals("A")) {
+					FightGateGuards(4);
+					
+				}
+				
+				else if (theirChoice3.equals("B")) {
+					LookAroundFindSewer();
+				}
+				
+			else if (theirChoice2.equals("B")) {
+				LookAroundFindSewer();
+				
+			}
+				
+			else if (theirChoice2.equals("C")) {
+				FightGateGuards(4);
+						
+			}
+		
+		else if (theirChoice.equals("C")) {
+			FightGateGuards(4);
 		}
+							
+				
+				
+			
+			
+			// so you either fight the guard, or run away
+			// fight guard means that coming to C will have one less guard
+			// run away, is same as LookArounndFindSewer();
+		
 		//Make referral to next page in accordance with choice
 		else if (theirChoice.equals("C")) {
 			System.out.println("");
@@ -46,6 +90,18 @@ public class Main {
 		// fight them
 		battler.Battle(protagonist, YoteyBoi);
 
+	}
+
+	private static void FightGateGuards() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void LookAroundFindSewer() {
+		System.out.println("");
+		// some text.
+		// ultimately find the sewer...
+		
 	}
 
 }
